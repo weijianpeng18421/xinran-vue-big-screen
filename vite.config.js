f22fd6ee1141c55957cca4d6bcfd53cc4277a5aa
魏建鹏 <784952975@qq.com>
2022-11-19 01:38:33 +0800

    增加server

--- vite.config.js
+++ vite.config.js
@@ -45,6 +45,20 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
+    // vite 相关配置
+    server: {
+        port: 5173,
+        host: true,
+        open: true,
+        proxy: {
+            // https://cn.vitejs.dev/config/#server-proxy
+            '/dev-api': {
+                target: 'http://localhost:5173',
+                changeOrigin: true,
+                rewrite: (p) => p.replace(/^\/dev-api/, '')
+            }
+        }
+    },
     transpileDependencies: [
         /\bvue-awesome\b/
     ],
